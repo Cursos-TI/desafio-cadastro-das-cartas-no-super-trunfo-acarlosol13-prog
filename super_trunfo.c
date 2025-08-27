633b6d3074aa68452f3d904f20d644cd630377bc
ANTONIO CARLOS DE OLIVEIRA <acarlosol13@gmail.com>
2025-08-27 17:50:44 -0300

    Create super_trunfo.c

---
+++ super_trunfo.c
@@ -0,0 +1,4 @@
+Atualiza super_trunfo.c para o nível aventureiro:
+- Calcula densidade populacional (população / área)
+- Calcula PIB per capita (PIB em reais / população)
+- Formata saída com 2 casas decimais
